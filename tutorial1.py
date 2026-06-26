daba66d188235eee3d1f76da265440c90c0c0a34
ViniPierre <pierre.vinicius@acad.espm.br>
2026-06-25 22:43:01 -0300

    Class number 2

---
+++ tutorial1.py
@@ -0,0 +1,27 @@
+# print('vini')
+
+# X = 'hello'
+# print (X)
+# X = 'bye'
+# print (X)
+
+# ---------------------------- #
+
+# print('Hello, what is your name?')
+# name = input()
+# print('Hello,',name)
+
+# num1 = 10
+# num2 = 10
+# print(num1 + num2)
+
+# print('Pick a number: ')
+# num1 = input()
+# print('Pick another number: ')
+# num2 = input()
+
+# X = int(num1) + int(num2)
+# print (X)
+
+# ---------------------------- #
+
