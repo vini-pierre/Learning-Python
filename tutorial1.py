afe23d1799b14326a18d232cd58bcb5138d4ba5c
ViniPierre <pierre.vinicius@acad.espm.br>
2026-07-06 15:32:06 -0300

    conditions

--- tutorial1.py
+++ tutorial1.py
@@ -25,3 +25,35 @@
 
 # ---------------------------- #
 
+# print (3 > 2)
+
+# print (2 > 3)
+
+# print ('hello' == 'helo')
+
+# print ('hello' != 'helo')
+
+# print ('hello' == 'Hello')
+
+# ---------------------------- #
+
+# if True:
+#     print('yes')
+
+# age = input('Qual é a sua idade: ')
+
+# if int(age) >= 18:
+#     print("You're able to drink and drive")
+# else:
+#     print("You're not able to drink and drive")
+
+# height = input('Qual é a sua altura (em metros)?: ')
+
+# if float(height) <= 1:
+#     print("you're can not drive, too small")
+# elif float(height) >= 2:
+#     print("you're can not drive, too tall")
+# else:
+#     print("you're can drive!")
+
+# ---------------------------- #
